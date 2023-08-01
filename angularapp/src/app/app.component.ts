4e9447fa5e03728fa50830ad818b045907eb5d22
Iamneo <techadmin@iamneo.ai>
2023-08-01 12:41:55 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -7,19 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   input:number=0;
-  output=0;
+  output=0.00;
   title = 'currency';
 
   base:any=0;
   to:any=0;
-  res='1.00'
+  res=0.00
 
   convert(c:any,a:any,b:any){
     this.input=c;
     this.base=Number(a);
     this.to=Number(b);
-     this.output= Math.round(this.input * (this.to/this.base));
-     this.res=this.output.toString().concat(".00").toString();
+     this.output= (this.input * (this.to/this.base));
+     this.res=this.output;
      console.log(this.base);
      console.log(this.to);
      console.log(this.input);
